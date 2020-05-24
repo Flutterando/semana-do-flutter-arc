1ebdf4e08dbd1a0aedbd9906f103539a41864af9
Jacob Moura <jacobaraujo7@gmail.com>
2020-05-24 20:04:17 -0300

    added

--- lib/app/models/user_model.dart
+++ lib/app/models/user_model.dart
@@ -5,4 +5,22 @@ class UserModel {
   final String picture;
 
   UserModel({this.name, this.email, this.nickname, this.picture});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      name: json['name'],
+      picture: json['picture'],
+      email: json['email'],
+      nickname: json['nickname'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['picture'] = this.picture;
+    data['email'] = this.email;
+    data['nickname'] = this.nickname;
+    return data;
+  }
 }
